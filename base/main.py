549a6c78df4a1eaff3c60b448a8c41794bfdd0d3
Trifyh <934472842@qq.com>
2024-10-17 22:05:38 +0800

    高dpi缩放适配

--- base/main.py
+++ base/main.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import (QWidget, QApplication)
-from PyQt5.QtGui import QIcon, QGuiApplication
+from PyQt5.QtGui import QFont
 import sys
 from UI.GuildleveExperienceCalculation import *
 
@@ -19,8 +19,12 @@ class MainLoop(QWidget):
 
 if __name__ == '__main__':
     # 自适应缩放和高dpi
-    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_DisableHighDpiScaling)
     # QGuiApplication.setAttribute(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
     app = QApplication(sys.argv)
+    font = QFont()
+    font.setPixelSize(12)  # 设置默认的像素字体大小
+    app.setFont(font)  # 设置应用程序的默认字体
+
     ex = MainLoop()
     sys.exit(app.exec_())
